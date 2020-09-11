dd7ce506f0fb00fb9e61d460f62d6afabc11562e
Peter Bispham <mail@peterbispham.com>
2020-09-11 15:29:36 +0100

    Admin creates user with no password and an email is sent

--- src/rard/users/forms.py
+++ src/rard/users/forms.py
@@ -16,6 +16,11 @@ class UserCreationForm(forms.UserCreationForm):
         {"duplicate_username": _("This username has already been taken.")}
     )
 
+    def __init__(self, *args, **kwargs):
+        super(UserCreationForm, self).__init__(*args, **kwargs)
+        self.fields['password1'].required = False
+        self.fields['password2'].required = False
+
     class Meta(forms.UserCreationForm.Meta):
         model = User
         fields = ('username', 'first_name', 'last_name', 'email',)
--- src/rard/users/tests/test_views.py
+++ src/rard/users/tests/test_views.py
@@ -1,16 +1,16 @@
 import pytest
+from django.contrib import messages
+from django.contrib.admin.sites import AdminSite
 from django.contrib.auth.models import AnonymousUser
 from django.http.response import Http404
 from django.test import RequestFactory
-from django.contrib import messages
 from django.urls import reverse
 
+from rard.users.admin import UserAdmin
+from rard.users.forms import UserCreationForm
 from rard.users.models import User
 from rard.users.tests.factories import UserFactory
-from rard.users.views import (
-    UserUpdateView,
-    user_detail_view,
-)
+from rard.users.views import UserUpdateView, user_detail_view
 
 pytestmark = pytest.mark.django_db
 
@@ -48,8 +48,10 @@ class TestUserUpdateView:
         request = rf.post(url)
         request.user = user
         setattr(request, 'data', data)
-        setattr(request, '_messages', messages.storage.default_storage(request))
-        response = UserUpdateView.as_view()(request)
+        setattr(
+            request, '_messages', messages.storage.default_storage(request)
+        )
+        UserUpdateView.as_view()(request)
 
         # should have single success message
         storage = messages.get_messages(request)
@@ -83,3 +85,30 @@ class TestUserDetailView:
 
         with pytest.raises(Http404):
             user_detail_view(request, username="username")
+
+
+class TestUserAdminCreateView:
+
+    # Test creation of new user via the admin interface
+
+    def test_admin_create_user(self, user: User, rf: RequestFactory):
+        request = rf.get("/admin/")
+        request.user = UserFactory()
+
+        site = AdminSite()
+        admin = UserAdmin(User, site)
+
+        d = {
+            'username': 'jsmith',
+            'email': 'jsmith@example.com'
+        }
+        new_user = User(**d)
+
+        form = UserCreationForm(data=d)
+        assert form.is_valid()  # create cleaned_data
+
+        admin.save_model(request, new_user, form, change=False)
+
+        # Though we haven't specified one, the admin sets a useable password
+        # so that the user can request a change
+        assert user.has_usable_password()
