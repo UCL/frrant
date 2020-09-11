d2b3a2181cc4850c6661a74a0c5246b5a75b3336
Peter Bispham <mail@peterbispham.com>
2020-09-11 15:30:32 +0100

    Enforce line length check with flake8 (except for settings files)

--- src/rard/users/urls.py
+++ src/rard/users/urls.py
@@ -1,8 +1,6 @@
 from django.urls import path
-from rard.users.views import (
-    user_detail_view,
-    user_update_view,
-)
+
+from rard.users.views import user_detail_view, user_update_view
 
 app_name = "users"
 urlpatterns = [
