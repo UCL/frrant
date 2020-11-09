7c59fa2f3345f9bbd9de112126426895148fabe8
Peter Bispham <mail@peterbispham.com>
2020-11-09 17:17:05 +0000

    flake8 changes

--- src/rard/research/views/mixins.py
+++ src/rard/research/views/mixins.py
@@ -69,17 +69,20 @@ class CanLockMixin:
 class SymbolContextMixin:
 
     # returns context info for the range of symbols available
-    
+
     def get_context_data(self, *args, **kwargs):
         context = super().get_context_data(*args, **kwargs)
         modifiers = [(x, 'Character %s' % x) for x in [
             'F0C3', 'F0C4', 'F0C5', 'F0C6', 'F0C7', 'F0C8', 'F0C9', 'F0CA',
-            'F0CB', 'F0CC', 'F0CD', 'F0CE', 'F0CF', 
+            'F0CB', 'F0CC', 'F0CD', 'F0CE', 'F0CF',
         ]]
-        chars = [(('%04x' % i).upper(), 'Character %04x' % i) for i in range(60501, 60581)]
-        more_chars = [(('%04x' % i).upper(), 'Character %04x' % i) for i in range(63665, 63743)]
-        # for i in range(60501, 60581):
-        #     chars.append(('%04x' % i).upper())
+        chars = [
+            (('%04x' % i).upper(), 'Character %04x' % i)
+            for i in range(60501, 60581)]
+        more_chars = [
+            (('%04x' % i).upper(), 'Character %04x' % i)
+            for i in range(63665, 63743)]
+
         character_sets = {
             'Character Set 1': chars,
             'Character Set 2': more_chars,
