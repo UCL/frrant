fd61060cfb4e249c45a922a1d5df6d4820155fe6
Tim Band <t.band>
2021-05-05 16:17:18 +0100

    Put new urls more in keeping with the old

--- src/rard/static/js/project.js
+++ src/rard/static/js/project.js
@@ -593,11 +593,11 @@ $('body').on('dragend', '.drag-item', function(event) {
 
 
 function moveLinkTo(post_data) {
-  runMoveAction(post_data, frrant_url.move_link)
+  runMoveAction(post_data, g_move_link_url)
 }
 
 function moveTopicTo(post_data) {
-    runMoveAction(post_data, frrant_url.move_topic)
+    runMoveAction(post_data, g_move_topic_url)
 }
 
 function runMoveAction(post_data, post_url) {
@@ -700,7 +700,7 @@ function refreshOriginalTextApparatusCriticus() {
         // so that it appears correct to the user. It would be all handled correctly
         // on saving anyway, but this is a cosmetic update so the user doesn't become confused!
         $.ajax({
-            url: frrant_url.refresh_original_text_content,
+            url: g_refresh_original_text_content_url,
             type: "POST",
             data: {'content': html},
             headers: headers,
