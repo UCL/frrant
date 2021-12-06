d86b49af55f44708307139bc1a6e0dfe4ead7673
tcouch <t.couch@ucl.ac.uk>
2021-12-06 10:15:55 +0000

    Distinguish development server emails from prod

--- src/config/settings/development.py
+++ src/config/settings/development.py
@@ -97,13 +97,13 @@ TEMPLATES[-1]["OPTIONS"]["loaders"] = [  # type: ignore[index] # noqa F405
 # https://docs.djangoproject.com/en/dev/ref/settings/#default-from-email
 DEFAULT_FROM_EMAIL = env(
     "DJANGO_DEFAULT_FROM_EMAIL",
-    default="Republican Antiquarians Research Database <noreply@example.com>",
+    default="Republican Antiquarians Research Database (dev) <noreply@example.com>",
 )
 # https://docs.djangoproject.com/en/dev/ref/settings/#server-email
 SERVER_EMAIL = env("DJANGO_SERVER_EMAIL", default=DEFAULT_FROM_EMAIL)
 # https://docs.djangoproject.com/en/dev/ref/settings/#email-subject-prefix
 EMAIL_SUBJECT_PREFIX = env(
-    "DJANGO_EMAIL_SUBJECT_PREFIX", default="[Republican Antiquarians Research Database]"
+    "DJANGO_EMAIL_SUBJECT_PREFIX", default="[Republican Antiquarians Research Database (dev)]"
 )
 
 # ADMIN
