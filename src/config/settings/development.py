682e78a5ca9f143007a952713c555169c4fb92f2
tcouch <t.couch@ucl.ac.uk>
2021-12-06 10:25:27 +0000

    Fix trailing whitespace

--- src/config/settings/development.py
+++ src/config/settings/development.py
@@ -103,7 +103,7 @@ DEFAULT_FROM_EMAIL = env(
 SERVER_EMAIL = env("DJANGO_SERVER_EMAIL", default=DEFAULT_FROM_EMAIL)
 # https://docs.djangoproject.com/en/dev/ref/settings/#email-subject-prefix
 EMAIL_SUBJECT_PREFIX = env(
-    "DJANGO_EMAIL_SUBJECT_PREFIX", 
+    "DJANGO_EMAIL_SUBJECT_PREFIX",
     default="[Republican Antiquarians Research Database (dev)]",
 )
 
