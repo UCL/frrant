f9ec084f09578f40da8e1cdafaea7b755d133924
acholyn <a.ho-lyn@ucl.ac.uk>
2023-04-19 16:06:31 +0100

    comment with short explanation

--- src/rard/research/migrations/0062_definite_possible_clarity.py
+++ src/rard/research/migrations/0062_definite_possible_clarity.py
@@ -6,61 +6,62 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('research', '0061_order_in_book'),
+        ("research", "0061_order_in_book"),
     ]
-
+    # the definite field for each link will be converted to definite_antiquarian.
+    # Definite work and book fields will be added
     operations = [
         migrations.AlterModelOptions(
-            name='book',
-            options={'ordering': ['unknown', 'order', 'number']},
+            name="book",
+            options={"ordering": ["unknown", "order", "number"]},
         ),
         migrations.AlterModelOptions(
-            name='worklink',
-            options={'ordering': ['work__unknown', 'order']},
+            name="worklink",
+            options={"ordering": ["work__unknown", "order"]},
         ),
         migrations.RenameField(
-            model_name='appositumfragmentlink',
-            old_name='definite',
-            new_name='definite_antiquarian',
+            model_name="appositumfragmentlink",
+            old_name="definite",
+            new_name="definite_antiquarian",
         ),
         migrations.RenameField(
-            model_name='fragmentlink',
-            old_name='definite',
-            new_name='definite_antiquarian',
+            model_name="fragmentlink",
+            old_name="definite",
+            new_name="definite_antiquarian",
         ),
         migrations.RenameField(
-            model_name='testimoniumlink',
-            old_name='definite',
-            new_name='definite_antiquarian',
+            model_name="testimoniumlink",
+            old_name="definite",
+            new_name="definite_antiquarian",
         ),
         migrations.AddField(
-            model_name='appositumfragmentlink',
-            name='definite_book',
+            model_name="appositumfragmentlink",
+            name="definite_book",
             field=models.BooleanField(default=False),
         ),
         migrations.AddField(
-            model_name='appositumfragmentlink',
-            name='definite_work',
+            model_name="appositumfragmentlink",
+            name="definite_work",
             field=models.BooleanField(default=False),
         ),
         migrations.AddField(
-            model_name='fragmentlink',
-            name='definite_book',
+            model_name="fragmentlink",
+            name="definite_book",
             field=models.BooleanField(default=False),
         ),
         migrations.AddField(
-            model_name='fragmentlink',
-            name='definite_work',
+            model_name="fragmentlink",
+            name="definite_work",
             field=models.BooleanField(default=False),
         ),
         migrations.AddField(
-            model_name='testimoniumlink',
-            name='definite_book',
+            model_name="testimoniumlink",
+            name="definite_book",
             field=models.BooleanField(default=False),
         ),
         migrations.AddField(
-            model_name='testimoniumlink',
-            name='definite_work',
+            model_name="testimoniumlink",
+            name="definite_work",
             field=models.BooleanField(default=False),
         ),
     ]
