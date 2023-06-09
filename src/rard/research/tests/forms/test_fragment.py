27ea7a39700ef72b4163081d89e1ac92e2410f07
tcouch <t.couch@ucl.ac.uk>
2023-06-09 11:23:22 +0100

    Fix brokoen form tests

--- src/rard/research/tests/forms/test_fragment.py
+++ src/rard/research/tests/forms/test_fragment.py
@@ -32,28 +32,39 @@ class TestFragmentCommentaryForm(TestCase):
 
 class TestFragmentLinkWorkForm(TestCase):
     def test_required_fields_no_antiquarian_selected(self):
-        # if no work specified we have no books to select
-        # and all the works are possible
-        form = FragmentLinkWorkForm(antiquarian=None, work=None)
+        # if no antiquarian specified we have no works to select
+        form = FragmentLinkWorkForm(
+            antiquarian=None,
+            work=None,
+            definite_antiquarian=False,
+            definite_work=False,
+            update=False,
+        )
         self.assertIsNone(form.fields["antiquarian"].initial)
         self.assertIsNone(form.fields["work"].initial)
-        self.assertTrue(form.fields["work"].disabled)
+        self.assertTrue(form.fields["work"].widget.attrs["disabled"])
         self.assertEqual(form.fields["work"].queryset.count(), 0)
-        self.assertTrue(form.fields["book"].disabled)
+        self.assertTrue(form.fields["book"].widget.attrs["disabled"])
         self.assertEqual(form.fields["book"].queryset.count(), 0)
 
     def test_required_fields_no_work_selected(self):
         antiquarian = Antiquarian.objects.create(name="Me", re_code=1)
         work = Work.objects.create(name="foo")
         antiquarian.works.add(work)
-        form = FragmentLinkWorkForm(antiquarian=antiquarian, work=None)
+        form = FragmentLinkWorkForm(
+            antiquarian=antiquarian,
+            work=None,
+            definite_antiquarian=False,
+            definite_work=False,
+            update=False,
+        )
         self.assertEqual(form.fields["antiquarian"].initial, antiquarian)
         self.assertIsNone(form.fields["work"].initial)
         self.assertFalse(form.fields["work"].disabled)
         self.assertEqual(
             form.fields["work"].queryset.count(), 2
         )  # includes unknown work
-        self.assertTrue(form.fields["book"].disabled)
+        self.assertTrue(form.fields["book"].widget.attrs["disabled"])
         self.assertEqual(form.fields["book"].queryset.count(), 0)
 
     def test_required_fields_with_work_selected(self):
@@ -64,7 +75,13 @@ class TestFragmentLinkWorkForm(TestCase):
         for i in range(0, NUM_BOOKS):
             Book.objects.create(number=i, work=work)
 
-        form = FragmentLinkWorkForm(antiquarian=antiquarian, work=work)
+        form = FragmentLinkWorkForm(
+            antiquarian=antiquarian,
+            work=work,
+            definite_antiquarian=False,
+            definite_work=False,
+            update=False,
+        )
         self.assertEqual(form.fields["antiquarian"].initial, antiquarian)
         self.assertEqual(form.fields["work"].initial, work)
         self.assertFalse(form.fields["book"].disabled)
