25e3c7f97851ed1c8292d3f88d7aff4e27471adf
Amanda Ho-Lyn <72266023+acholyn@users.noreply.github.com>
2023-08-08 13:53:22 +0100

    Revert "Feature/text editor updates"

--- src/rard/static/js/setDefiniteValues.js
+++ src/rard/static/js/setDefiniteValues.js
@@ -1,4 +1,5 @@
 function setValues(e, fieldName) {
+  console.log(e.target.value, e.target.checked);
   var checkboxValue = e.target.closest("input[type='checkbox']").checked;
   document.querySelectorAll(`input[name=${fieldName}]`).forEach((input) => {
     input.checked = checkboxValue;
