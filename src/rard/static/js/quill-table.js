af59358439eaa5cc71e72526ec92543e162094e1
acholyn <a.ho-lyn@ucl.ac.uk>
2023-10-03 13:49:08 +0100

    trying to prevent keyboard input on tables

--- src/rard/static/js/quill-table.js
+++ src/rard/static/js/quill-table.js
@@ -345,6 +345,15 @@ class Table extends Module {
                 .forEach((tableCell) => {
                   tableCell.addEventListener("dblclick", editContents);
                   tableCell.addEventListener("click", updateCurrentCell);
+                  // prevent keyboard input
+                  tableCell.addEventListener("keydown", function (e) {
+                    e.preventDefault();
+                    console.log(e);
+                  });
+                  tableCell.addEventListener("keypress", function (e) {
+                    e.preventDefault();
+                    console.log(e);
+                  });
                 });
             }
           }
