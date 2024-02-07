b171d908d99fbeafce2fc035f039bd671363b123
tcouch <t.couch@ucl.ac.uk>
2024-02-07 13:57:04 +0000

    Update to remove need for fragment_testimonia_mentions list

--- src/rard/research/migrations/0067_add_mentioned_in_fk_field.py
+++ src/rard/research/migrations/0067_add_mentioned_in_fk_field.py
@@ -6,11 +6,11 @@ from django.db import migrations, models, IntegrityError
 from psycopg2 import errors
 
 
-def get_fragment_testimonia_mentions(self, apps):
+def get_fragment_testimonia_mentions(text_object_field, apps):
     """This finds all mentions of (A)F&T, identifies the
     original objects and returns them as a list"""
 
-    soup = bs4.BeautifulSoup(self.content, features="html.parser")
+    soup = bs4.BeautifulSoup(text_object_field.content, features="html.parser")
     mentions = soup.find_all("span", class_="mention")
     valid_models = ["fragment", "anonymousfragment", "testimonium"]
     linked_items = []
@@ -26,23 +26,18 @@ def get_fragment_testimonia_mentions(self, apps):
                 pass
     return linked_items
 
-
-def set_mentions(self, apps):
-    found_mention_items = get_fragment_testimonia_mentions(self, apps)
-    self.fragment_testimonia_mentions = []
+def set_mentions(text_object_field, apps):
+    found_mention_items = get_fragment_testimonia_mentions(text_object_field, apps)
     for item in found_mention_items:
-        if self not in item.mentioned_in.all():
+        if text_object_field not in item.mentioned_in.all():
             try:
-                item.mentioned_in.add(self)
-                self.fragment_testimonia_mentions.append(item)
+                item.mentioned_in.add(text_object_field)
             except IntegrityError as e:
                 if isinstance(e.__cause__, errors.UniqueViolation):
                     print(f"Duplicate entry detected: {e}")
                     pass
                 else:
                     raise
-    return self.fragment_testimonia_mentions
-
 
 def update_mentions(apps, schema_editor):
     TextObjectField = apps.get_model("research", "TextObjectField")
