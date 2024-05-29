68da4cb17ed55c47738f00ef0e536569f4d142e1
acholyn <a.ho-lyn@ucl.ac.uk>
2024-05-29 12:20:10 +0100

    updating duplication to fix apcrit refs in ot + test

--- src/rard/research/views/fragment.py
+++ src/rard/research/views/fragment.py
@@ -1,3 +1,4 @@
+from bs4 import BeautifulSoup
 from django.contrib.auth.context_processors import PermWrapper
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 from django.core.exceptions import BadRequest, ObjectDoesNotExist
@@ -1256,6 +1257,23 @@ def duplicate_fragment(request, pk, model_name):
                 ] = new_original_text  # make sure to assign new OT in place of the old one (field skipped above)
 
                 model_class.objects.create(**new_model_data)
+
+        # use beautifulsoup to update the references in the ot content
+        soup = BeautifulSoup(new_original_text.content, features="html.parser")
+        mentions = soup.find_all(
+            "span", class_="mention", attrs={"data-denotation-char": "#"}
+        )
+        apcriti = new_original_text.apparatus_criticus_items.all()
+
+        for mention, apcrit in zip(mentions, apcriti):
+            mention["data-id"] = apcrit.pk
+            mention["data-original-text"] = new_original_text.pk
+            mention["data-parent"] = new_original_text.pk
+
+        updated_content = str(soup)
+
+        new_original_text.content = updated_content
+        new_original_text.save()
 
     # Create a new fragment with the same values as original
     new_fragment_data = {}
