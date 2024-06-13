08b1875064042cd5aa0837a4330bbc33b6b8efa4
acholyn <a.ho-lyn@ucl.ac.uk>
2024-06-13 10:34:24 +0100

    basis for new concordance model

--- src/rard/research/models/original_text.py
+++ src/rard/research/models/original_text.py
@@ -146,6 +146,25 @@ class Concordance(HistoryModelMixin, BaseModel):
         return "%s: %s" % (self.source, self.identifier)
 
 
+class ConcordanceModel(HistoryModelMixin, BaseModel):
+    edition_identifier = models.CharField(max_length=128, blank=False)
+    ant_work_identifier = models.CharField(max_length=128, blank=True)
+    display_order = models.CharField(max_length=30, blank=True)
+
+    TYPE_CHOICES = {
+        "F": "Fragment",
+        "T": "Testimonium",
+        "App": "Appendix",
+        "P.": "Pagination",
+        "NA": "N/A",
+    }
+    content_type = models.Choices(choices=TYPE_CHOICES)
+    reference = models.CharField(max_length=15, blank=True)
+    concordance_order = models.CharField(
+        max_length=15
+    )  # maybe add a validator so only numbers and . allowed?
+
+
 class Translation(HistoryModelMixin, BaseModel):
     history = HistoricalRecords()
 
