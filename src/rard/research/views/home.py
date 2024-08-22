5abbaf8c7b5103cc005377ba9cd612a03d7006af
acholyn <a.ho-lyn@ucl.ac.uk>
2024-08-22 15:45:35 +0100

    only displaying lock icon if authenticated user

--- src/rard/research/views/home.py
+++ src/rard/research/views/home.py
@@ -3,7 +3,4 @@ from django.views.generic import TemplateView
 
 class HomeView(TemplateView):
     def get_template_names(self):
-        if not self.request.user.is_authenticated:
-            return ["pages/front.html"]
-        else:
-            return ["research/home.html"]
+        return ["research/home.html"]
