48b55b0bc8ab08de53f324fa1b0791d3982ad2de
Tim Band <t.b@ucl>
2026-05-14 12:27:52 +0100

    Serious work towards #494 speed up Varro

--- src/rard/research/templatetags/get_item.py
+++ src/rard/research/templatetags/get_item.py
@@ -6,3 +6,8 @@ register = template.Library()
 @register.filter
 def get_item(item, selector):
     return item.get(selector, "")
+
+
+@register.filter
+def get_item_list(item, selector):
+    return item.get(selector, [])
--- src/rard/research/templatetags/links_for_work.py
+++ src/rard/research/templatetags/links_for_work.py
@@ -4,17 +4,6 @@ register = template.Library()
 
 
 @register.filter
-def fragment_links_for_work(antiquarian, work):
-    args = {}
-    if work:
-        args["work"] = work
-    else:
-        args["work__isnull"] = True
-
-    return antiquarian.fragmentlinks.filter(**args).order_by("work_order")
-
-
-@register.filter
 def testimonium_links_for_work(antiquarian, work):
     args = {}
     if work:
@@ -26,11 +15,25 @@ def testimonium_links_for_work(antiquarian, work):
 
 
 @register.filter
-def appositum_links_for_work(antiquarian, work):
-    args = {}
-    if work:
-        args["work"] = work
-    else:
-        args["work__isnull"] = True
+def fragment_links_by_work(antiquarian):
+    return get_links_by_work(antiquarian.fragmentlinks, "fragment")
 
-    return antiquarian.appositumfragmentlinks.filter(**args).order_by("work_order")
+
+@register.filter
+def testimonium_links_by_work(antiquarian):
+    return get_links_by_work(antiquarian.testimoniumlinks, "testimonium")
+
+
+@register.filter
+def appositum_links_by_work(antiquarian):
+    return get_links_by_work(antiquarian.appositumfragmentlinks, "anonymous_fragment")
+
+
+def get_links_by_work(links, related):
+    work_to_object = {}
+    for obj in links.order_by("work_order").select_related(related):
+        w = obj.work_id
+        if w not in work_to_object:
+            work_to_object[w] = []
+        work_to_object[w].append(obj)
+    return work_to_object
